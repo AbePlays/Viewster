8bb85a4ca66acc50ec841294b5142588c5d59ff5
Abe <abhi.rawat456@gmail.com>
2020-10-15 12:28:49 +0530

    Added code to navigate between screens

--- lib/screens/BottomNavigator/BottomNavigator.dart
+++ lib/screens/BottomNavigator/BottomNavigator.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:viewster/screens/FavoriteMovies.dart';
+import 'package:viewster/screens/FavoriteShows.dart';
+import 'package:viewster/screens/Home.dart';
+import 'package:viewster/screens/Profile.dart';
 
 class BottomNavigator extends StatefulWidget {
   @override
@@ -6,10 +10,53 @@ class BottomNavigator extends StatefulWidget {
 }
 
 class _BottomNavigatorState extends State<BottomNavigator> {
+  int currentIndex = 0;
+
+  List<Widget> screenList = [
+    Home(),
+    FavoriteShows(),
+    FavoriteMovies(),
+    Profile()
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Bottom"),
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: screenList[currentIndex],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        type: BottomNavigationBarType.fixed,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.orange,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.local_movies),
+            title: Text("Shows"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.movie),
+            title: Text("Movies"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            title: Text("Account"),
+          ),
+        ],
+      ),
     );
   }
 }
