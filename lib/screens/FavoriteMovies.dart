30064dc6901cb370bda2812f32dc9c7feb08b18b
Abe <abhi.rawat456@gmail.com>
2020-10-17 19:27:19 +0530

    Added Favorite Movies Screen

--- lib/screens/FavoriteMovies.dart
+++ lib/screens/FavoriteMovies.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:viewster/provider/FavoritesProvider.dart';
+import 'package:viewster/screens/Search/SearchCard.dart';
 
 class FavoriteMovies extends StatefulWidget {
   @override
@@ -12,10 +13,47 @@ class _FavoriteMoviesState extends State<FavoriteMovies> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Consumer<FavoritesProvider>(
-      builder: (context, favs, child) {
-        return Text("${favs.favMovies.length}");
-      },
-    ));
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "Favorite Movies",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(
+            child: Consumer<FavoritesProvider>(
+              builder: (context, favs, child) {
+                if (favs.favMovies.length == 0) {
+                  return Text("Favorites Empty");
+                } else {
+                  return ListView.separated(
+                      scrollDirection: Axis.vertical,
+                      itemBuilder: (context, index) {
+                        return SearchCard(
+                          imageUrl: favs.favMovies[index].imageUrl,
+                          language: favs.favMovies[index].language,
+                          releaseDate: favs.favMovies[index].releaseDate,
+                          title: favs.favMovies[index].title,
+                          votes: favs.favMovies[index].votes,
+                        );
+                      },
+                      separatorBuilder: (context, index) {
+                        return SizedBox(
+                          height: 10,
+                        );
+                      },
+                      itemCount: favs.favMovies.length);
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
