0fc62de0ce3383d81c55635dc0a829a7462b361b
Abe <abhi.rawat456@gmail.com>
2020-10-18 09:01:40 +0530

    Added Live Trending Movies and Shows

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:viewster/screens/Search/Search.dart';
 import 'package:viewster/screens/Trending/Trending.dart';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
   @override
@@ -8,6 +10,48 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List trendingShows = [];
+  List trendingMovies = [];
+
+  void getTrendingMovies() async {
+    String url =
+        'https://api.themoviedb.org/3/trending/movie/week?api_key=6458ca648b70d6d3d574f8e0b2ce817d';
+    print(url);
+
+    try {
+      var response = await http.get(url);
+      var jsonResponse = convert.jsonDecode(response.body);
+      setState(() {
+        trendingMovies = jsonResponse['results'];
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void getTrendingShows() async {
+    String url =
+        'https://api.themoviedb.org/3/trending/tv/week?api_key=6458ca648b70d6d3d574f8e0b2ce817d';
+    print(url);
+
+    try {
+      var response = await http.get(url);
+      var jsonResponse = convert.jsonDecode(response.body);
+      setState(() {
+        trendingShows = jsonResponse['results'];
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTrendingShows();
+    getTrendingMovies();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -75,7 +119,9 @@ class _HomeState extends State<Home> {
           SizedBox(
             height: 10,
           ),
-          Trending(),
+          Trending(
+            list: trendingShows,
+          ),
           SizedBox(
             height: 20,
           ),
@@ -98,7 +144,9 @@ class _HomeState extends State<Home> {
           SizedBox(
             height: 10,
           ),
-          Trending()
+          Trending(
+            list: trendingMovies,
+          )
         ],
       ),
     );
--- lib/screens/Trending/Trending.dart
+++ lib/screens/Trending/Trending.dart
@@ -1,32 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Trending extends StatefulWidget {
+  final List list;
+  Trending({this.list});
+
   @override
   _TrendingState createState() => _TrendingState();
 }
 
 class _TrendingState extends State<Trending> {
-  List posters = [
-    'assets/images/bttf.jpg',
-    'assets/images/panther.jpg',
-    'assets/images/venom.jpeg'
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 300,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 3,
+        itemCount: widget.list.isEmpty ? 0 : 5,
         itemBuilder: (context, index) {
           return Padding(
             padding: const EdgeInsets.all(10.0),
             child: ClipRRect(
               borderRadius: BorderRadius.circular(20),
-              child: Image(
-                image: AssetImage(posters[index]),
-              ),
+              child: FadeInImage.assetNetwork(
+                  placeholder: 'assets/images/notfound.png',
+                  image:
+                      "https://image.tmdb.org/t/p/w185${widget.list[index]['poster_path']}"),
             ),
           );
         },
